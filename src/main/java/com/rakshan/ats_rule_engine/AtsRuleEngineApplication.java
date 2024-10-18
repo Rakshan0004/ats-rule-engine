7382bfb4c5f8adbe5a6449833b370646eec34cdc
Chintala Rakshan <136550641+Rakshan0004@users.noreply.github.com>
2024-10-18 14:49:54 +0530

    first commit

---
+++ src/main/java/com/rakshan/ats_rule_engine/AtsRuleEngineApplication.java
@@ -0,0 +1,13 @@
+package com.rakshan.ats_rule_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsRuleEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtsRuleEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rakshan/ats_rule_engine/AtsRuleEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakshan.ats_rule_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsRuleEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
